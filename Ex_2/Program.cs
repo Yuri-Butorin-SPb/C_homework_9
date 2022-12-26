cde301e999c90f265f96d7e6ce1a038b6262ada5
Yuri <butor.yuri@gmail.com>
2022-12-26 15:33:59 +0300

    add second exercise

---
+++ Ex_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int m = int.Parse(Console.ReadLine());
+
+int result = 0;
+int sum = PrintDigits(n, m);
+Console.WriteLine(sum);
+
+
+int PrintDigits(int n, int m)
+{
+
+
+    if (m >= n)
+    {
+        result += m;
+        m--;
+        PrintDigits(n, m);
+    }
+
+    return result;
+}
