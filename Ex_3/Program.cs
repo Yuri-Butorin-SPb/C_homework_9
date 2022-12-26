abe3036b1cb7070d32f061434537dc215885a8de
Yuri <butor.yuri@gmail.com>
2022-12-26 15:35:01 +0300

    add third exercise

--- Ex_3/Program.cs
+++ Ex_3/Program.cs
@@ -3,13 +3,7 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
-// функция ack(n, m)
-//    если n = 0
-//      вернуть m + 1
-//    иначе, если m = 0
-//      вернуть ack (n - 1, 1)
-//    еще
-//      вернуть ack(n - 1, ack (n, m - 1))
+
 
 Console.WriteLine("Введите число: ");
 int n = int.Parse(Console.ReadLine());
